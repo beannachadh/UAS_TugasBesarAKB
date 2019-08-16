3651dbddc8040d25d6220eb3c9dda9d9ab4cbc30
Garry Prang <37956977+beannachadh@users.noreply.github.com>
2019-08-17 02:00:15 +0700

    Add files via upload

---
+++ app/build/generated/source/source/apt/debug/io/realm/com_example_franscisco_tubesuas_model_TemanModelRealmProxyInterface.java
@@ -0,0 +1,21 @@
+package io.realm;
+
+
+public interface com_example_franscisco_tubesuas_model_TemanModelRealmProxyInterface {
+    public Integer realmGet$id();
+    public void realmSet$id(Integer value);
+    public Integer realmGet$nim();
+    public void realmSet$nim(Integer value);
+    public Integer realmGet$telp();
+    public void realmSet$telp(Integer value);
+    public String realmGet$nama();
+    public void realmSet$nama(String value);
+    public String realmGet$kelas();
+    public void realmSet$kelas(String value);
+    public String realmGet$email();
+    public void realmSet$email(String value);
+    public String realmGet$instagram();
+    public void realmSet$instagram(String value);
+    public String realmGet$facebook();
+    public void realmSet$facebook(String value);
+}
